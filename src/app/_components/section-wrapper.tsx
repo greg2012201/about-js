cd5a55d86b6781d569493b3e40be68dfd9bc1321
Grzegorz Dubiel <greg2012201@gmail.com>
2023-12-30 10:05:41 +0000

    Add reusable wrapper component for sections.

---
+++ src/app/_components/section-wrapper.tsx
@@ -0,0 +1,19 @@
+import React, { ReactNode } from "react";
+import { motion } from "framer-motion";
+import { ClassNameValue, twMerge } from "tailwind-merge";
+
+type Props = { children: ReactNode; className?: ClassNameValue };
+
+function SectionWrapper({ children, className }: Props) {
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      className={twMerge("h-[300px] pt-8", className)}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default SectionWrapper;
