77c356a686370af349627f32ee2dd5f8f8ef9295
Grzegorz Dubiel <greg2012201@gmail.com>
2024-01-02 22:22:35 +0000

    Add section wrapper to the Next section.

--- src/app/_components/sections/next.tsx
+++ src/app/_components/sections/next.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import { motion } from "framer-motion";
 import React from "react";
 import Title from "../title";
 import Italic from "../italic";
+import SectionWrapper from "../section-wrapper";
+import IconsLine from "../icons-line";
 
 function Next() {
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      whileInView={{ opacity: 1 }}
-      className="min-h-screen"
-    >
+    <SectionWrapper renderIconLine={<IconsLine iconSrc="./next-js-logo.svg" />}>
       <Title>Fresh news about Next.js</Title>
       <Italic>News and outstanding toturials, guides and insights.</Italic>
-    </motion.div>
+    </SectionWrapper>
   );
 }
 
