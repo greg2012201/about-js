25a42cbd7b05df0833a2a7770b34cdc35baed54f
Grzegorz Dubiel <greg2012201@gmail.com>
2024-01-02 21:55:57 +0000

    Line drawing animation.

---
+++ src/app/_components/icons-line.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Image from "next/image";
+import { motion } from "framer-motion";
+
+type Props = {
+  iconSrc: string;
+};
+
+function IconsLine({ iconSrc }: Props) {
+  return (
+    <div className="bg-red flex h-full  w-[40px] flex-col items-center justify-start space-y-2 pb-2">
+      <motion.div
+        initial={{ opacity: 0 }}
+        transition={{ delay: 0.3, duration: 0.4 }}
+        whileInView={{ opacity: 1 }}
+      >
+        <Image
+          alt="next.js framework logo"
+          width={30}
+          height={30}
+          src={iconSrc}
+        />
+      </motion.div>
+      <motion.div
+        initial={{ scaleY: 0 }}
+        whileInView={{ scaleY: 1 }}
+        transition={{ delay: 0.5, duration: 0.8 }}
+        className="h-full w-1 origin-top bg-white"
+      />
+    </div>
+  );
+}
+
+export default IconsLine;
