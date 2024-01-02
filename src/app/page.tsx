ef5c7904d9e89a0a214a42cf5e8a5b80715bfde7
Grzegorz Dubiel <greg2012201@gmail.com>
2024-01-02 22:09:51 +0000

    Remove spacing from page wrapper.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Intro from "./_components/sections/intro";
 import Next from "./_components/sections/next";
+import React from "./_components/sections/react";
 
 export default function Home() {
   return (
-    <div className="space-y-8 p-4 text-white">
+    <div className="w-full p-4 text-white">
       <Intro />
       <Next />
+      <React />
     </div>
   );
 }
