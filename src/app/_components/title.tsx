9da67a16f718e958972e4a8e32616acd72d0960d
Grzegorz Dubiel <greg2012201@gmail.com>
2024-01-04 01:10:32 +0000

    Generate gradient for title randomly.

--- src/app/_components/title.tsx
+++ src/app/_components/title.tsx
@@ -1,10 +1,17 @@
+import generateRandomGradient from "@/utils/generate-random-gradient";
+
 type Props = {
   children: string;
 };
 
 function Title({ children }: Props) {
   return (
-    <h2 className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-2xl font-semibold text-transparent">
+    <h2
+      className={`${generateRandomGradient(
+        ["indigo-500", "purple-500", "pink-500"],
+        "to-r",
+      )} bg-clip-text text-2xl font-semibold text-transparent`}
+    >
       {children}
     </h2>
   );
