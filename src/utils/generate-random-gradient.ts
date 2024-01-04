b78e951d4978b530b741b20ebb73d63ab99247a5
Grzegorz Dubiel <greg2012201@gmail.com>
2024-01-04 01:08:53 +0000

    Make function flexible.

--- src/utils/generate-random-gradient.ts
+++ src/utils/generate-random-gradient.ts
@@ -1,11 +1,7 @@
-function generateRandomGradient(colors: string[]): string {
+function generateRandomGradient(colors: string[], direction = "to-t"): string {
   const shuffledColors = [...colors].sort(() => Math.random() - 0.5);
-  const gradient = `bg-gradient-to-t from-${shuffledColors[0]} via-${shuffledColors[1]} to-${shuffledColors[2]}`;
+  const gradient = `bg-gradient-${direction} from-${shuffledColors[0]} via-${shuffledColors[1]} to-${shuffledColors[2]}`;
 
-  console.log(
-    "🚀 ~ file: generate-random-gradient.ts:7 ~ generateRandomGradient ~ gradient:",
-    gradient,
-  );
   return gradient;
 }
 
