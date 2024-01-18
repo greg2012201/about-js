bbaf612aeab9fd6dea460c3d5488aaaf092b798d
root <root@DESKTOP-U9TG690>
2024-01-18 23:43:15 +0100

    Allow to use custom className in BaseText component.

--- src/app/_components/base-text.tsx
+++ src/app/_components/base-text.tsx
@@ -1,9 +1,14 @@
+import { ClassNameValue, twMerge } from "tailwind-merge";
+
 type Props = {
   children: string;
+  className?: ClassNameValue;
 };
 
-function BaseText({ children }: Props) {
-  return <p className="text-base font-light text-white">{children}</p>;
+function BaseText({ children, className }: Props) {
+  return (
+    <p className={twMerge(`text-xl  text-slate-300`, className)}>{children}</p>
+  );
 }
 
 export default BaseText;
