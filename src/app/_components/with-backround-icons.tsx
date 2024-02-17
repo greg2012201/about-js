19c24776fbba3a412975ca9e64f873c4c0edb0cc
Grzegorz Dubiel <greg2012201@gmail.com>
2024-02-18 00:28:54 +0100

    Add animate node.js icon.

--- src/app/_components/with-backround-icons.tsx
+++ src/app/_components/with-backround-icons.tsx
@@ -1,7 +1,8 @@
 "use client";
 
+import { motion } from "framer-motion";
 import Image from "next/image";
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 
 type Props = {
   children: ReactNode;
@@ -10,18 +11,28 @@ type Props = {
 function WithBackgroundIcons({ children }: Props) {
   return (
     <div className="relative w-screen">
-      <div className="absolute left-0 top-0 flex flex-col opacity-10">
+      <div className="absolute left-0 top-0 flex flex-col space-y-60 pt-48 opacity-10">
+        <motion.div
+          animate={{ x: 50, scale: 1.2 }}
+          transition={{
+            ease: "easeOut",
+            duration: 28,
+            repeatType: "reverse",
+            repeat: Infinity,
+          }}
+        >
+          <Image
+            alt="big node.js icon"
+            width={800}
+            height={800}
+            src="/nodejs-icon.svg"
+          />
+        </motion.div>
         <Image
           alt="big react icon"
-          width={600}
-          height={600}
+          width={800}
+          height={800}
           src="/react-2.svg"
-        />
-        <Image
-          alt="big node.js icon"
-          width={600}
-          height={600}
-          src="/nodejs-icon.svg"
         />
       </div>
       {children}
