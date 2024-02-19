88a904520767093e20e8745056622801189ba221
Grzegorz Dubiel <greg2012201@gmail.com>
2024-02-19 23:59:56 +0100

    Adjust icons.

--- src/app/_components/with-backround-icons.tsx
+++ src/app/_components/with-backround-icons.tsx
@@ -11,7 +11,7 @@ type Props = {
 function WithBackgroundIcons({ children }: Props) {
   return (
     <div className="relative w-screen">
-      <div className="absolute left-0 top-0 flex flex-col space-y-60 pt-48 opacity-10">
+      <div className="absolute left-0 top-0 flex w-screen flex-col space-y-40 pt-20 opacity-10 sm:space-y-60 sm:pt-48 lg:pl-40 lg:pr-60 ">
         <motion.div
           animate={{ x: 50, scale: 1.2 }}
           transition={{
@@ -23,19 +23,20 @@ function WithBackgroundIcons({ children }: Props) {
         >
           <Image
             alt="big node.js icon"
-            width={800}
-            height={800}
+            width={900}
+            height={900}
             src="/nodejs-icon.svg"
           />
         </motion.div>
         <motion.div
+          className="max-w-[1000px] self-end"
           animate={{ rotate: 360 }}
           transition={{ duration: 100, repeat: Infinity, ease: "linear" }}
         >
           <Image
             alt="big react icon"
-            width={800}
-            height={800}
+            width={1000}
+            height={1000}
             src="/react-2.svg"
           />
         </motion.div>
