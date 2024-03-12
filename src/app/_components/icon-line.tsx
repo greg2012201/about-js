6713889c1bb7aafe5210bf9e30465e31bafae1fa
Grzegorz Dubiel <greg2012201@gmail.com>
2024-03-13 00:06:19 +0100

    Improve icon line animations.

--- src/app/_components/icon-line.tsx
+++ src/app/_components/icon-line.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 type Props = {
   iconSrc: string;
@@ -9,13 +10,27 @@ type Props = {
 };
 
 function IconLine({ iconSrc, title }: Props) {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+  });
+  const scaleY = useSpring(useTransform(scrollYProgress, [1, 0], [0, 1]), {
+    stiffness: 400,
+    damping: 120,
+  });
+
   return (
-    <div className="flex h-full  w-[40px] flex-col items-center justify-between space-y-2 pb-2 md:space-y-4 lg:space-y-6">
+    <div
+      ref={containerRef}
+      className="flex h-full  w-[40px] flex-col items-center justify-between space-y-2 pb-2 md:space-y-4 lg:space-y-6"
+    >
       <motion.div
         className="z-20 h-[30px] w-[30px] md:w-[40px] lg:w-[45px]"
         initial={{ opacity: 0 }}
         transition={{ delay: 0.3, duration: 0.4 }}
         whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
       >
         <Image
           alt={`a logo of ${title}`}
@@ -26,9 +41,7 @@ function IconLine({ iconSrc, title }: Props) {
         />
       </motion.div>
       <motion.div
-        initial={{ scaleY: 0 }}
-        whileInView={{ scaleY: 1 }}
-        transition={{ delay: 0.5, duration: 0.8 }}
+        style={{ scaleY }}
         className={`z-20 h-full w-1 origin-top rounded-md bg-gradient-to-t from-pink-500 via-purple-500 to-indigo-500`}
       />
     </div>
