0aa00053d0b17cf1f317cbfdc7525daffeb064c3
Grzegorz Dubiel <greg2012201@gmail.com>
2024-03-18 23:47:44 +0100

    Allow classNames from props.

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -1,13 +1,20 @@
 import { ReactNode } from "react";
 import BaseText from "./base-text";
+import { ClassNameValue, twMerge } from "tailwind-merge";
 
 type Props = {
   children: ReactNode;
+  className?: ClassNameValue;
 };
 
-function Card({ children }: Props) {
+function Card({ children, className }: Props) {
   return (
-    <div className=" relative z-50 h-full w-full overflow-hidden rounded-2xl border border-transparent bg-gradient-to-br from-indigo-800 to-pink-800/[0.2]  group-hover:border-slate-700">
+    <div
+      className={twMerge(
+        className,
+        "relative z-50 h-full w-full overflow-hidden rounded-2xl border border-transparent bg-gradient-to-br from-indigo-800 to-pink-800/[0.2]  group-hover:border-slate-700",
+      )}
+    >
       <div className="h-full rounded-2xl p-4 bg-dot-white/[0.2]">
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-slate-800  [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
         <div className="relative z-50">
