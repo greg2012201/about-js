c32df2bc7c3d53e944d6186504ae60bc02cf5a35
Grzegorz Dubiel <greg2012201@gmail.com>
2024-03-18 23:48:13 +0100

    Add Card.

--- src/app/_components/sections/next.tsx
+++ src/app/_components/sections/next.tsx
@@ -5,6 +5,7 @@ import Title from "../title";
 import BaseText from "../base-text";
 import SectionWrapper from "../section-wrapper";
 import IconLine from "../icon-line";
+import Card from "../card";
 
 function Next() {
   return (
@@ -12,7 +13,12 @@ function Next() {
       renderIconLine={<IconLine title="Next.js" iconSrc="./next-js-logo.svg" />}
     >
       <Title>Fresh news about Next.js</Title>
-      <BaseText>News and outstanding tutorials, guides and insights.</BaseText>
+      <Card className="max-w-[1000px]">
+        <BaseText className="text-2xl  md:text-[28px]">
+          There are many new things out there. It is good to know what&apos;s
+          going on in the new API&apos;s 😉
+        </BaseText>
+      </Card>
     </SectionWrapper>
   );
 }
