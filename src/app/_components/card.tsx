b10ec917efe690b7160b6a8065434a25ef60bea1
Grzegorz Dubiel <greg2012201@gmail.com>
2024-03-19 23:34:45 +0100

    Merga class in correct place/

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -9,16 +9,11 @@ type Props = {
 
 function Card({ children, className }: Props) {
   return (
-    <div
-      className={twMerge(
-        className,
-        "relative z-50 h-full w-full overflow-hidden rounded-2xl border border-transparent bg-gradient-to-br from-indigo-800 to-pink-800/[0.2]  group-hover:border-slate-700",
-      )}
-    >
+    <div className="relative z-50 h-full w-full overflow-hidden rounded-2xl border border-transparent bg-gradient-to-br from-indigo-800 to-pink-800/[0.2] group-hover:border-slate-700">
       <div className="h-full rounded-2xl p-4 bg-dot-white/[0.2]">
         <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-slate-800  [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
         <div className="relative z-50">
-          <div className="p-4">{children}</div>
+          <div className={twMerge(className, `p-4`)}>{children}</div>
         </div>
       </div>
     </div>
