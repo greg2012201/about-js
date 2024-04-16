4f813a7c37c957640af6969f0b4dcedb6b5b4d8d
Grzegorz Dubiel <greg2012201@gmail.com>
2024-04-16 23:41:21 +0200

    Add and more... list.

--- src/app/_components/sections/and-more.tsx
+++ src/app/_components/sections/and-more.tsx
@@ -1,10 +1,31 @@
 "use client";
 
-import React from "react";
+import React, { ReactElement } from "react";
 import SectionWrapper from "../section-wrapper";
 import Title from "../title";
 import IconLine from "../icon-line";
 import BaseText from "../base-text";
+import { GrTest } from "react-icons/gr";
+import { FaCss3 } from "react-icons/fa";
+import { FaCode } from "react-icons/fa";
+import { IoServerSharp } from "react-icons/io5";
+import { GrSend } from "react-icons/gr";
+import { FaGithub } from "react-icons/fa";
+import Card from "../card";
+
+type FeatureListitemProps = {
+  children: string;
+  icon: ReactElement;
+};
+
+function FeatureListsItem({ children, icon }: FeatureListitemProps) {
+  return (
+    <li className="flex items-center space-x-2">
+      {icon}
+      <p className="text-xl  italic">{children}</p>
+    </li>
+  );
+}
 
 function AndMore() {
   return (
@@ -18,11 +39,28 @@ function AndMore() {
       }
     >
       <Title>Something missing?</Title>
-      <BaseText className="max-w-3xl">
-        No worries! JS world moves really fast and there are more interesting
-        stuff to talk about. Like: test projects, css, other frontend
-        frameworks, backend frameworks, news, packages and open source.
-      </BaseText>
+      <Card
+        wrapperClassName="mt-auto mb-auto"
+        contentClassName="flex max-w-[1000px] flex-col space-y-8"
+      >
+        <BaseText className="max-w-3xl pt-4">
+          No worries! JS world moves really fast and there are more interesting
+          stuff to talk about, like:
+        </BaseText>
+
+        <ul className="space-y-1 pl-8 ">
+          <FeatureListsItem icon={<GrTest />}>test projects</FeatureListsItem>
+          <FeatureListsItem icon={<FaCss3 />}>CSS</FeatureListsItem>
+          <FeatureListsItem icon={<FaCode />}>
+            other frontend frameworks
+          </FeatureListsItem>
+          <FeatureListsItem icon={<IoServerSharp />}>
+            backend frameworks
+          </FeatureListsItem>
+          <FeatureListsItem icon={<GrSend />}>news</FeatureListsItem>
+          <FeatureListsItem icon={<FaGithub />}>open source</FeatureListsItem>
+        </ul>
+      </Card>
     </SectionWrapper>
   );
 }
