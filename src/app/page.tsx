b15d3cd2aaaf9795bd40c0113db08bb392dfdbf2
Grzegorz Dubiel <greg2012201@gmail.com>
2024-04-19 23:26:44 +0200

    Move features component to the about page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,47 +11,6 @@ export default function Home() {
   return (
     <div className="mx-auto flex w-full flex-col items-center p-4 pb-0 text-white ">
       <Intro />
-      <WithBackgroundIcons>
-        <div className="mx-auto flex w-full flex-col items-center p-4 text-white ">
-          <Next
-            renderContent={
-              <Code
-                theme="material-theme-ocean"
-                lang="powershell"
-                code={dedent`
-npx create-next-app@latest
-What is your project named? cool-project 🤔
-Would you like to use TypeScript? No / Yes
-Which blog about Next.js would you like to read? ***about-js*** 😎`}
-              />
-            }
-          />
-          <React
-            renderContent={
-              <Code
-                theme="material-theme-ocean"
-                lang="jsx"
-                code={dedent`
-                'use what!?'
-                
-                 function AboutJS() {
-                  return (
-                    <div>
-                      <h1>Greetings!</h1>
-                      <p>let's see what we got here.</p>
-                    </div>
-                  );
-                }
-                
-                export default AboutJS;
-                `}
-              />
-            }
-          />
-          <NodeJS />
-          <AndMore />
-        </div>
-      </WithBackgroundIcons>
     </div>
   );
 }
