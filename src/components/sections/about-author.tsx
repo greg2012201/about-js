90bf3368ab834cac80c6878b1b5f4d67fd9cb454
Grzegorz Dubiel <49715864+greg2012201@users.noreply.github.com>
2024-04-21 00:31:04 +0200

    Prevent crash.

--- src/components/sections/about-author.tsx
+++ src/components/sections/about-author.tsx
@@ -5,7 +5,7 @@ function AboutAuthor() {
   return (
     <div className="flex w-full max-w-[930px] flex-col space-y-4">
       <Title>Who am I?</Title>
-      <BaseText>
+      <BaseText>{`
         I love creating new things by coding. I have always liked "front side"
         of the cyber world. I have been coding since march of 2020. Before, I've
         always been curious about how user interfaces work behind the scenes and
@@ -13,7 +13,7 @@ function AboutAuthor() {
         fascinated in React, before I met this library I have been coding in
         vanilla JS. When I started learning React I felt tons of possibilities.
         I'm a big fan of Next.js with React Server Components. I like taking on
-        difficult programming challenges.
+        difficult programming challenges.`}
       </BaseText>
     </div>
   );
