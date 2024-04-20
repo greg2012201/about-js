3a25a0860ce749bf254327da10c2fa58c997941d
Grzegorz Dubiel <greg2012201@gmail.com>
2024-04-20 23:12:06 +0200

    Allow providing custom class.

--- src/components/brand-title.tsx
+++ src/components/brand-title.tsx
@@ -1,19 +1,31 @@
 import React from "react";
+import { ClassNameValue, twMerge } from "tailwind-merge";
 type Props = {
   children: string;
+  className?: ClassNameValue;
 };
 
-export function Subtitle({ children }: Props) {
+export function Subtitle({ children, className }: Props) {
   return (
-    <p className="bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-2xl text-transparent md:text-3xl">
+    <p
+      className={twMerge(
+        className,
+        `bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 bg-clip-text text-2xl text-transparent md:text-3xl`,
+      )}
+    >
       {children}
     </p>
   );
 }
 
-export function Title({ children }: Props) {
+export function Title({ children, className }: Props) {
   return (
-    <h1 className="text-5xl leading-tight md:text-7xl xl:text-[80px] ">
+    <h1
+      className={twMerge(
+        className,
+        `text-5xl leading-tight md:text-7xl xl:text-[80px]`,
+      )}
+    >
       {children}
     </h1>
   );
