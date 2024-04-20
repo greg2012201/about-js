0985b3256e9b4c8147732801dd09b17faae956aa
Grzegorz Dubiel <greg2012201@gmail.com>
2024-04-20 22:13:48 +0200

    Cleanup.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import Code from "@/components/code";
-import AndMore from "@/components/sections/and-more";
 import Intro from "@/components/sections/intro";
-import Next from "@/components/sections/next";
-import NodeJS from "@/components/sections/node-js";
 import React from "@/components/sections/react";
-import WithBackgroundIcons from "@/components/with-backround-icons";
-import dedent from "dedent";
 
 export default function Home() {
   return (
