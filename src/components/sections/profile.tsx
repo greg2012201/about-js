344a1d7ca30bc4e49a283c22abbcfd4f08a9404b
Grzegorz Dubiel <greg2012201@gmail.com>
2024-04-23 23:38:56 +0200

    Fix typo.

--- src/components/sections/profile.tsx
+++ src/components/sections/profile.tsx
@@ -34,7 +34,7 @@ function Profile() {
         <Avatar className="hidden sm:block" />
       </div>
       <div className="flex flex-col sm:space-y-1">
-        <Title>Wlcome👋</Title>
+        <Title>Welcome👋</Title>
         <Subtitle>I am Grzegorz Dubiel</Subtitle>
         <Avatar className="mt-6 sm:hidden" />
       </div>
