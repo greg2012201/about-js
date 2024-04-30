18451f385511510a4626b761cf20ddc8556a0754
Grzegorz Dubiel <greg2012201@gmail.com>
2024-04-30 22:55:30 +0200

    Add className for Title.

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,10 +1,18 @@
+import { ClassNameValue, twMerge } from "tailwind-merge";
+
 type Props = {
   children: string;
+  className?: ClassNameValue;
 };
 
-function Title({ children }: Props) {
+function Title({ children, className }: Props) {
   return (
-    <h2 className={`text-3xl font-semibold text-indigo-400  md:text-5xl`}>
+    <h2
+      className={twMerge(
+        className,
+        `text-3xl font-semibold text-indigo-400  md:text-5xl`,
+      )}
+    >
       {children}
     </h2>
   );
