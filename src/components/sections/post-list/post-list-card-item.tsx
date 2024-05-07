0b58b17166ae3106243dfca24bf66d58d6c7e3a4
Grzegorz Dubiel <greg2012201@gmail.com>
2024-05-07 23:38:57 +0200

    Create Author component.

--- src/components/sections/post-list/post-list-card-item.tsx
+++ src/components/sections/post-list/post-list-card-item.tsx
@@ -1,11 +1,35 @@
 import { Post } from "@/utils/getPosts";
 import Image from "next/image";
-import { twMerge } from "tailwind-merge";
+import { ClassNameValue, twMerge } from "tailwind-merge";
+
+type AuthorProps = Pick<
+  Post["data"],
+  "author" | "createdAt" | "authorAvatar"
+> & { className?: ClassNameValue };
+
+function Author({ author, authorAvatar, createdAt, className }: AuthorProps) {
+  console.log("author", author);
+  return (
+    <div className={twMerge(`flex items-start space-x-2 py-2`, className)}>
+      <Image
+        src={authorAvatar}
+        width={50}
+        height={50}
+        alt="author avatar"
+        className="rounded-full"
+      />
+      <div className="flex flex-col">
+        <p className="text-xl font-bold text-pink-400">{author}</p>
+        <p className="text-xs italic text-slate-300">{createdAt}</p>
+      </div>
+    </div>
+  );
+}
 
 type PostListCardItemProps = Pick<Post, "data" | "excerpt"> & { index: number };
 
 function PostListCardItem({ data, excerpt, index }: PostListCardItemProps) {
-  const { title, slug, image } = data;
+  const { title, slug, image, author, authorAvatar, createdAt } = data;
   const isEven = index % 2 === 0;
   const imageDynamicClassName = isEven
     ? "sm:rounded-none sm:rounded-bl-md sm:rounded-tl-md"
@@ -24,9 +48,14 @@ function PostListCardItem({ data, excerpt, index }: PostListCardItemProps) {
       />
       <div className="flex flex-grow flex-col space-y-2 p-3">
         <header className="flex flex-col">
-          <h5 className="text-3xl font-bold">{title}</h5>
+          <h5 className="text-3xl font-bold text-slate-300">{title}</h5>
           <span className="mt-1 h-[7px] max-w-[80px] -skew-x-12 bg-purple-500" />
         </header>
+        <Author
+          author={author}
+          authorAvatar={authorAvatar}
+          createdAt={createdAt}
+        />
         <p>{excerpt ?? ""}</p>
       </div>
     </li>
