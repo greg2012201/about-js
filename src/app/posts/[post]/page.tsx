e2c173ceaf13b8362fdfef2583e5af662fe3bdd1
Grzegorz Dubiel <greg2012201@gmail.com>
2024-05-28 00:09:06 +0200

    Set up post page.

--- src/app/posts/[post]/page.tsx
+++ src/app/posts/[post]/page.tsx
@@ -1,4 +1,5 @@
-import { getAllPostSlugs } from "@/utils/getPosts";
+import transformPost from "@/markdown/transform-post";
+import { getAllPostSlugs, getPost } from "@/utils/getPosts";
 import React from "react";
 
 type Props = {
@@ -6,14 +7,22 @@ type Props = {
     post: string;
   };
 };
-
+const higlhliterClassName = `p-2 text-sm [&>pre]:overflow-x-auto [&>pre]:p-4 [&>pre]:leading-snug  [&_code]:block [&_code]:max-w-[100px] [&_code>span::before]:content-[counter(step)]`;
 export async function generateStaticParams() {
   const allPostSlugs = await getAllPostSlugs();
+
   return allPostSlugs.map((postSlug) => ({ post: postSlug }));
 }
 
-function Post({ params: { post } }: Props) {
-  return <div>{post}</div>;
+async function Post({ params: { post } }: Props) {
+  const postData = await getPost(post);
+  const postHTML = await transformPost(postData.content);
+  return (
+    <div
+      className={higlhliterClassName}
+      dangerouslySetInnerHTML={{ __html: postHTML }}
+    />
+  );
 }
 
 export default Post;
