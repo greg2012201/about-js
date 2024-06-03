751d6c52763c9ef8fa6ae560b675af13cc13b8e5
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-03 23:27:18 +0200

    Add post list to the posts page.

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import PostList from "@/components/sections/post-list";
 
 function Posts() {
-  return <div>Posts</div>;
+  return (
+    <div className="flex w-full justify-center pt-6">
+      <PostList />
+    </div>
+  );
 }
 
 export default Posts;
