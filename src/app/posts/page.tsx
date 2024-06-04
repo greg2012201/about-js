297d17c880c06abae545511ae6e744d727ca81db
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-04 23:06:01 +0200

    Add padding.

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -2,7 +2,7 @@ import PostList from "@/components/sections/post-list";
 
 function Posts() {
   return (
-    <div className="flex w-full justify-center pt-6">
+    <div className="flex w-full justify-center px-4 pt-6">
       <PostList />
     </div>
   );
