689e9c467dc6b1dc869c1f893ec076d9833c6726
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-11 23:28:25 +0200

    Less space in snippet header.

--- src/markdown/with-shiki/templates.ts
+++ src/markdown/with-shiki/templates.ts
@@ -30,7 +30,7 @@ export const headerTemplate = ({ lang, codeBlockId }: HeaderProps) => {
   const renderIcon = LANG_ICONS_MAP.get(lang);
   const buttonId = short.generate();
   return dedent`
-  <div className="header flex items-center border-b-[1px] border-slate-500 px-6 py-4 text-neutral-400">
+  <div className="header flex items-center border-b-[1px] border-slate-500 px-6 py-1 text-neutral-400">
     <div className="flex flex-grow items-center space-x-1">
       ${renderIcon}
       <p className="text-sm font-semibold capitalize ">
@@ -61,7 +61,7 @@ export const wrapperTemplate = ({
   const codeBlockId = short.generate();
 
   return dedent`
-  <div className="flex flex-col rounded-md border-[1px] border-slate-500 bg-[#0f111a]">
+  <div className="flex flex-col rounded-md border-[1px] border-slate-500 bg-[#0f111a] my-6">
     ${headerTemplate({ lang, codeBlockId })}
     <div id="${codeBlockId}" className="${twMerge(
       "p-2 text-sm [&>pre]:overflow-x-auto [&>pre]:p-4 [&>pre]:leading-snug  [&_code]:block [&_code]:max-w-[100px]",
