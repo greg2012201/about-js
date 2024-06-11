6ee5f3d34f26ad7768bba272943a83dfe1796d44
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-11 23:29:41 +0200

    Keep consistent class names conception in html templates.

--- src/markdown/with-shiki/templates.ts
+++ src/markdown/with-shiki/templates.ts
@@ -30,14 +30,14 @@ export const headerTemplate = ({ lang, codeBlockId }: HeaderProps) => {
   const renderIcon = LANG_ICONS_MAP.get(lang);
   const buttonId = short.generate();
   return dedent`
-  <div className="header flex items-center border-b-[1px] border-slate-500 px-6 py-1 text-neutral-400">
-    <div className="flex flex-grow items-center space-x-1">
+  <div class="header flex items-center border-b-[1px] border-slate-500 px-6 py-1 text-neutral-400">
+    <div class="flex flex-grow items-center space-x-1">
       ${renderIcon}
-      <p className="text-sm font-semibold capitalize ">
+      <p class="text-sm font-semibold capitalize ">
           ${transformLang(lang)}
       </p>    
     </div>
-    <button id="${buttonId}" className="copy-button h-[30px]" onclick="handleCopy('${codeBlockId}', '${buttonId}')" type="button">
+    <button id="${buttonId}" class="copy-button h-[30px]" onclick="handleCopy('${codeBlockId}', '${buttonId}')" type="button">
       ${mdOutlineDone}${faCopy}
     </button>
   </div>
@@ -55,17 +55,17 @@ export const wrapperTemplate = ({
   children,
   lang = "javascript",
 }: Props): string => {
-  const lineNumbersClassName = ["powershell", "bash", "console"].includes(lang)
+  const lineNumbersclass = ["powershell", "bash", "console"].includes(lang)
     ? "[&_code>span::before]:content-['$']"
     : "[&_code>span::before]:content-[counter(step)]";
   const codeBlockId = short.generate();
 
   return dedent`
-  <div className="flex flex-col rounded-md border-[1px] border-slate-500 bg-[#0f111a] my-6">
+  <div class="flex flex-col rounded-md border-[1px] border-slate-500 bg-[#0f111a] my-6">
     ${headerTemplate({ lang, codeBlockId })}
-    <div id="${codeBlockId}" className="${twMerge(
+    <div id="${codeBlockId}" class="${twMerge(
       "p-2 text-sm [&>pre]:overflow-x-auto [&>pre]:p-4 [&>pre]:leading-snug  [&_code]:block [&_code]:max-w-[100px]",
-      lineNumbersClassName,
+      lineNumbersclass,
     )}">${children}</div>
   </div>
 `;
