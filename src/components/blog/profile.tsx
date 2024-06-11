9817b17cbc3dc208af99fa5546dd7bc900b7d0f2
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-11 23:16:49 +0200

    Wrap profile with link.

--- src/components/blog/profile.tsx
+++ src/components/blog/profile.tsx
@@ -1,5 +1,6 @@
 import { Post } from "@/utils/getPosts";
 import Image from "next/image";
+import Link from "next/link";
 import { ClassNameValue, twMerge } from "tailwind-merge";
 
 type ProfileProps = Pick<
@@ -18,26 +19,29 @@ function Profile({
     size === "default"
       ? "grid grid-cols-[50px_1fr] grid-rows-2"
       : "flex flex-col items-center";
-  const authorTextSize = size === "default" ? "text-xl" : "text-2xl";
+  const authorTextSize =
+    size === "default" ? "text-xl" : "text-2xl text-center";
   return (
-    <div className={twMerge(`space-x-2 py-2`, wrapperClassName, className)}>
-      <Image
-        src={authorAvatar}
-        width={size === "default" ? 50 : 80}
-        height={size === "default" ? 50 : 80}
-        alt="author avatar"
-        className="row-span-2 rounded-full"
-      />
-      <p
-        className={twMerge(
-          `row-span-1 text-center font-bold text-pink-400`,
-          authorTextSize,
-        )}
-      >
-        {author}
-      </p>
-      <p className="row-span-2 text-xs italic text-slate-300">{createdAt}</p>
-    </div>
+    <Link href="/about">
+      <div className={twMerge(`space-x-2 py-2`, wrapperClassName, className)}>
+        <Image
+          src={authorAvatar}
+          width={size === "default" ? 50 : 80}
+          height={size === "default" ? 50 : 80}
+          alt="author avatar"
+          className="row-span-2 rounded-full"
+        />
+        <p
+          className={twMerge(
+            `row-span-1  font-bold text-pink-400`,
+            authorTextSize,
+          )}
+        >
+          {author}
+        </p>
+        <p className="row-span-2 text-xs italic text-slate-300">{createdAt}</p>
+      </div>
+    </Link>
   );
 }
 
