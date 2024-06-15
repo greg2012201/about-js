c01dbef9457271fd4d4289ae28087e3d2fe1b0e3
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-15 22:10:37 +0200

    Autolink headers.

--- src/markdown/transform-post.ts
+++ src/markdown/transform-post.ts
@@ -3,11 +3,15 @@ import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import { remark } from "remark";
 import withShiki from "./with-shiki";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 async function transformPost(content: string) {
   const file = await remark()
     .use(remarkParse)
     .use(remarkRehype, { allowDangerousHtml: true })
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings)
     .use(withShiki)
     .use(rehypeStringify)
     .process(content);
