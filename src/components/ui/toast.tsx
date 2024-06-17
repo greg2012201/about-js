936c62ab601f044236107523b2106bb00d824a5e
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-18 00:17:44 +0200

    Update toast width.

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -16,7 +16,7 @@ const ToastViewport = React.forwardRef<
   <ToastPrimitives.Viewport
     ref={ref}
     className={cn(
-      "fixed left-[50%] top-0 z-[100] flex max-h-screen w-full translate-x-[-50%] flex-col-reverse p-4  md:max-w-[420px]",
+      "fixed left-[50%] top-0 z-[100] flex translate-x-[-50%] flex-col-reverse p-4",
       className,
     )}
     {...props}
