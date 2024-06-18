9bac61776f2ee794b377582f81d1332924598cbb
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-18 22:23:04 +0200

    Optimize Share component.

--- src/components/blog/share.tsx
+++ src/components/blog/share.tsx
@@ -1,34 +1,44 @@
 "use client";
 
 import type { TableOfContentsItem } from "@/markdown/types";
-import { useEffect } from "react";
+import { MouseEvent, useCallback, useEffect } from "react";
 import { useToast } from "../ui/use-toast";
 
 type Props = { tocList: TableOfContentsItem[] };
 
 function Share({ tocList }: Props) {
   const { toast } = useToast();
+  const handleHeadingClick = useCallback((event: Event) => {
+    const target = event.target as HTMLElement;
+    navigator.clipboard
+      .writeText(
+        `${window.location.origin}${window.location.pathname}#${target.id}`,
+      )
+      .then(() => {
+        toast({
+          title: "Link copied",
+          description: "Thanks for sharing!",
+          duration: 1500,
+        });
+      })
+      .catch((err) => console.error("Failed to copy:", err));
+  }, []);
   useEffect(() => {
+    const headingElements: HTMLElement[] = [];
     tocList.forEach(({ href }: TableOfContentsItem) => {
       const headingElement = document.getElementById(href.replace("#", ""));
       if (!headingElement) {
         return;
       }
-      headingElement.addEventListener("click", () => {
-        navigator.clipboard
-          .writeText(
-            `${window.location.origin}${window.location.pathname}${href}`,
-          )
-          .then(() => {
-            toast({
-              title: "Link copied",
-              description: "Thanks for sharing!",
-              duration: 1500,
-            });
-          })
-          .catch((err) => console.error("Failed to copy:", err));
-      });
+      headingElements.push(headingElement);
     });
+    headingElements.forEach((headingElement) =>
+      headingElement.addEventListener("click", handleHeadingClick),
+    );
+    return () =>
+      headingElements.forEach((headingElement) =>
+        headingElement.removeEventListener("click", handleHeadingClick),
+      );
   }, []);
   return null;
 }
