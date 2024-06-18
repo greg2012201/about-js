3a7f517f08da53150a36d35d46f444c399f8de8b
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-18 23:41:40 +0200

    Simplify nav.

--- src/components/nav-list.tsx
+++ src/components/nav-list.tsx
@@ -1,59 +1,23 @@
+"use client";
+
 import type { Prettify } from "@/types";
-import { motion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
-type ListItemWrapperProps = {
-  className?: string;
-  isMotion?: boolean;
-  children: string;
-  order: number;
-};
-
-function ListItemWrapper({
-  className,
-  isMotion,
-  children,
-  order = 0,
-}: ListItemWrapperProps) {
-  return isMotion ? (
-    <motion.li
-      initial={{ x: 200, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      transition={{ type: "spring", duration: 0.5, delay: order / 24 }}
-      className={className}
-    >
-      {children}
-    </motion.li>
-  ) : (
-    <li className={className}> {children}</li>
-  );
-}
-
 type Props = Prettify<{
   navConfig: Record<"label" | "path", string>[];
-  handleItemClick: VoidFunction;
   horizontal?: boolean;
   className?: string;
   isAnimated?: boolean;
 }>;
 
-function NavList({
-  navConfig,
-  handleItemClick,
-  horizontal = false,
-  className,
-  isAnimated,
-}: Props) {
+function NavList({ navConfig, horizontal = false, className }: Props) {
   const pathname = usePathname();
-  const layoutClass = horizontal
-    ? "flex flex-row gap-x-8 text-xl font-bold"
-    : "flex flex-col text-2xl";
 
   return (
     <nav className={className}>
-      <ul className={twMerge("space-y-2 sm:space-y-0", layoutClass)}>
+      <ul className="flex h-full flex-row flex-wrap items-center gap-x-1 font-bold  sm:gap-x-8 sm:text-xl">
         {navConfig.map(({ label, path }, index) => {
           const isActive = pathname === path;
 
@@ -61,14 +25,10 @@ function NavList({
             ? "text-purple-500 border-accent"
             : "text-white hover:text-purple-500 border-transparent";
           return (
-            <Link key={label} onClick={handleItemClick} href={path}>
-              <ListItemWrapper
-                order={index}
-                isMotion={isAnimated}
-                className={twMerge("p-1", activeClass)}
-              >
+            <Link key={label} href={path}>
+              <li key={index} className={twMerge("p-1", activeClass)}>
                 {label}
-              </ListItemWrapper>
+              </li>
             </Link>
           );
         })}
--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -1,9 +1,3 @@
-"use client";
-
-import { useCallback, useEffect, useState } from "react";
-import { CgMenu } from "react-icons/cg";
-import Sidebar from "./sidebar";
-import useClickOutside from "@/hooks/use-click-outside";
 import Logo from "./logo";
 import NavList from "./nav-list";
 
@@ -14,59 +8,13 @@ const NAV_CONFIG = [
 ];
 
 function TopBar() {
-  const [navOpen, setNavOpen] = useState(false);
-  const ref = useClickOutside<HTMLDivElement>(() => setNavOpen(false));
-  const handleClose = useCallback(() => setNavOpen(false), []);
-
-  useEffect(() => {
-    const handleEscape = (e: KeyboardEvent) => {
-      if (e.key === "Escape") {
-        setNavOpen(false);
-      }
-    };
-    if (navOpen) {
-      document.addEventListener("keyup", handleEscape);
-    }
-
-    return () => {
-      document.removeEventListener("keyup", handleEscape);
-    };
-  }, [navOpen]);
-
   return (
     <>
-      <div className="flex h-[50px] w-full items-center justify-between bg-[rgba(0,0,0,0.47)] p-2 shadow-md sm:h-[60px]">
+      <div className="flex min-h-[40px] w-full flex-wrap items-center justify-between bg-[rgba(0,0,0,0.47)] p-2 shadow-md sm:min-h-[60px]">
         <Logo />
-        <button
-          onClick={() => {
-            setNavOpen((prevState) => !prevState);
-          }}
-        >
-          <CgMenu
-            className={`block text-[34px] text-purple-500 [@media(min-width:640px)]:hidden`}
-          />
-        </button>
 
-        <NavList
-          handleItemClick={handleClose}
-          navConfig={NAV_CONFIG}
-          className="hidden [@media(min-width:640px)]:flex"
-          horizontal
-        />
+        <NavList navConfig={NAV_CONFIG} horizontal />
       </div>
-      <Sidebar
-        ref={ref}
-        open={navOpen}
-        handleClose={handleClose}
-        className="block [@media(min-width:640px)]:hidden"
-        navList={
-          <NavList
-            isAnimated={navOpen}
-            handleItemClick={handleClose}
-            navConfig={NAV_CONFIG}
-          />
-        }
-      />
     </>
   );
 }
