be5a48832401dbb6d293be04b9de8382f483149f
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-24 23:42:24 +0200

    Wrap ContentContainer with Card component.

--- src/components/sections/content-container.tsx
+++ src/components/sections/content-container.tsx
@@ -4,6 +4,8 @@ import SectionWrapper from "../section-wrapper";
 import Image from "next/image";
 import BaseText from "../base-text";
 import { twMerge } from "tailwind-merge";
+import Card from "../card";
+import IconLine from "../icon-line";
 
 type Props = {
   title: string;
@@ -23,42 +25,44 @@ function ContentContainer({
 }: Props) {
   return (
     <SectionWrapper className="w max-w-1000px h-fit">
-      <div
-        className={twMerge(
-          `flex flex-col space-y-2 sm:grid  sm:grid-rows-2 sm:space-x-4 sm:space-y-0`,
-          orientation === "left"
-            ? "sm:grid-cols-[400px_1fr]"
-            : "sm:grid-cols-[1fr_400px]",
-        )}
-      >
-        <Image
-          src={src}
+      <Card contentClassName="flex max-w-[1000px] space-y-8">
+        <div
           className={twMerge(
-            `row-span-2 self-center sm:w-full`,
-            orientation === "left" ? "col-start-1" : "col-start-2",
-          )}
-          alt={alt}
-          width={300}
-          height={100}
-        />
-        <Title
-          className={twMerge(
-            "row-start-1",
-            orientation === "left" ? "col-start-2" : "col-start-1",
-          )}
-          size="large"
-        >
-          {title}
-        </Title>
-        <BaseText
-          className={twMerge(
-            "row-start-2",
-            orientation === "left" ? "col-start-2" : "col-start-1",
+            `flex flex-col space-y-2 sm:grid  sm:grid-rows-2 sm:space-x-4 sm:space-y-0`,
+            orientation === "left"
+              ? "sm:grid-cols-[400px_1fr]"
+              : "sm:grid-cols-[1fr_400px]",
           )}
         >
-          {content}
-        </BaseText>
-      </div>
+          <Image
+            src={src}
+            className={twMerge(
+              `row-span-2 self-center sm:w-full`,
+              orientation === "left" ? "col-start-1" : "col-start-2",
+            )}
+            alt={alt}
+            width={300}
+            height={100}
+          />
+          <Title
+            className={twMerge(
+              "row-start-1",
+              orientation === "left" ? "col-start-2" : "col-start-1",
+            )}
+            size="large"
+          >
+            {title}
+          </Title>
+          <BaseText
+            className={twMerge(
+              "row-start-2",
+              orientation === "left" ? "col-start-2" : "col-start-1",
+            )}
+          >
+            {content}
+          </BaseText>
+        </div>
+      </Card>
     </SectionWrapper>
   );
 }
