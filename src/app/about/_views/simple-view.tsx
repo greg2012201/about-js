61eae222a560399e26f83de292e1a8f45dccbe87
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-25 22:45:18 +0200

    Bring back icons again.

--- src/app/about/_views/simple-view.tsx
+++ src/app/about/_views/simple-view.tsx
@@ -11,41 +11,43 @@ function SimpleView() {
       <Profile />
       <AboutAuthor />
       <Separator />
-      <div className="mx-auto flex w-full flex-col items-center space-y-28 p-4 text-white ">
-        <ContentContainer
-          title="Frontend"
-          content="Fresh new about frameworks tooling. We all know that a new library pop
+      <WithBackgroundIcons>
+        <div className="mx-auto flex w-full flex-col items-center space-y-28 p-4 text-white ">
+          <ContentContainer
+            title="Frontend"
+            content="Fresh new about frameworks tooling. We all know that a new library pop
           out every day. You can be sure that with this blog you will be up to
           date."
-          orientation="left"
-          imageConfig={{
-            src: "./react-2.svg",
-            alt: "logos of the frontend technologies",
-          }}
-        />
-        <ContentContainer
-          title="Backend"
-          content="Fresh new about frameworks tooling. We all know that a new library pop
+            orientation="left"
+            imageConfig={{
+              src: "./react-2.svg",
+              alt: "logos of the frontend technologies",
+            }}
+          />
+          <ContentContainer
+            title="Backend"
+            content="Fresh new about frameworks tooling. We all know that a new library pop
           out every day. You can be sure that with this blog you will be up to
           date."
-          orientation="right"
-          imageConfig={{
-            src: "./nodejs-icon.svg",
-            alt: "logos of the frontend technologies",
-          }}
-        />
-        <ContentContainer
-          title="Web dev Fullstack"
-          content="Fresh new about frameworks tooling. We all know that a new library pop
+            orientation="right"
+            imageConfig={{
+              src: "./nodejs-icon.svg",
+              alt: "logos of the frontend technologies",
+            }}
+          />
+          <ContentContainer
+            title="Web dev Fullstack"
+            content="Fresh new about frameworks tooling. We all know that a new library pop
           out every day. You can be sure that with this blog you will be up to
           date."
-          orientation="left"
-          imageConfig={{
-            src: "./nodejs-icon.svg",
-            alt: "logos of the frontend technologies",
-          }}
-        />
-      </div>
+            orientation="left"
+            imageConfig={{
+              src: "./nodejs-icon.svg",
+              alt: "logos of the frontend technologies",
+            }}
+          />
+        </div>
+      </WithBackgroundIcons>
     </>
   );
 }
