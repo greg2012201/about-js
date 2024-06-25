82ce790cf4cfffe3af5433e957e7814245dc0453
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-25 23:03:04 +0200

    Format code.

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -15,7 +15,7 @@ function Card({ children, contentClassName, wrapperClassName }: Props) {
       <div
         className={`relative z-50 h-fit w-full overflow-hidden rounded-2xl border border-transparent bg-gradient-to-br from-indigo-800 to-pink-800/[0.2] group-hover:border-slate-700`}
       >
-        <div className="h-full self-center rounded-2xl p-4 bg-dot-white/[0.2]">
+        <div className="bg-dot-white/[0.2] h-full self-center rounded-2xl p-4">
           <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-slate-800  [mask-image:radial-gradient(ellipse_at_center,transparent_10%,black)]"></div>
           <div className="relative z-50">
             <div className={twMerge(contentClassName, `p-4`)}>{children}</div>
