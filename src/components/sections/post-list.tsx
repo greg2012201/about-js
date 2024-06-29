fae4737b7a44481379fe298bcdcc9c0246b54ea8
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-29 22:43:53 +0200

    cleanup.

--- src/components/sections/post-list.tsx
+++ src/components/sections/post-list.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Title from "@/components/title";
 import SectionWrapper from "@/components/section-wrapper";
 import PostListComponent from "@/components/post-list";
 
