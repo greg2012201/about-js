1ff5a2604d429829351b12753825fd886652253a
Grzegorz Dubiel <greg2012201@gmail.com>
2024-06-29 22:50:26 +0200

    Simplify title.

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -5,7 +5,7 @@ import SectionWrapper from "@/components/section-wrapper";
 function Posts() {
   return (
     <div className="flex flex-col items-center space-y-4 py-4">
-      <Title className="mb-12">📖 Posts 📖</Title>
+      <Title className="mb-12">Posts</Title>
       <SectionWrapper className="h-fit">
         <PostList />
       </SectionWrapper>
