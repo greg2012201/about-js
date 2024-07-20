f6d3d2de2a80804ac29ec1b1cf5da5c871edc31d
Grzegorz Dubiel <greg2012201@gmail.com>
2024-07-20 22:54:21 +0200

    Cleanup.

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -7,7 +7,6 @@ function TopBar() {
     <>
       <div className="flex min-h-[40px] w-full flex-wrap items-center justify-center bg-[rgba(0,0,0,0.47)] p-2 shadow-md sm:min-h-[60px] [@media(min-width:282px)]:justify-between">
         <Logo />
-
         <NavList navConfig={NAV_CONFIG} />
       </div>
     </>
