1a806847233c449c3ac30d9ce7b64a4064885880
Grzegorz Dubiel <greg2012201@gmail.com>
2024-07-27 01:24:02 +0200

    Use default locale const.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,12 @@
 import createMiddleware from "next-intl/middleware";
-import { LOCALES } from "./constants/translations";
+import { DEFAULT_LOCALE, LOCALES } from "./constants/translations";
 
 export default createMiddleware({
   // A list of all locales that are supported
   locales: LOCALES,
 
   // Used when no locale matches
-  defaultLocale: "en",
+  defaultLocale: DEFAULT_LOCALE,
 });
 export const config = {
   matcher: ["/", "/(about|posts|home)/:path*", "/(pl|en)/:path*"],
