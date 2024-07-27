a984b19272faad0d64463a18b59d7b109fcf7429
Grzegorz Dubiel <greg2012201@gmail.com>
2024-07-27 23:00:37 +0200

    Add missing type checker function.

--- src/types.ts
+++ src/types.ts
@@ -3,3 +3,6 @@ import { LOCALES } from "./constants/translations";
 export type Prettify<T> = { [K in keyof T]: T[K] };
 
 export type Locale = (typeof LOCALES)[number];
+
+export const isLocale = (maybeLocale: any): maybeLocale is Locale =>
+  LOCALES.includes(maybeLocale);
