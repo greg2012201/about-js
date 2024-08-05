8e72b6e89ed62fb1e91ba4d98d2f826e11fbad39
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-05 23:01:36 +0200

    Add accent color to the check icon.

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -131,7 +131,7 @@ const SelectItem = React.forwardRef<
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
-        <Check className="h-4 w-4" />
+        <Check className="h-4 w-4" color="#a855f7" />
       </SelectPrimitive.ItemIndicator>
     </span>
 
