ae0121f5f887bf715ee2201106af1d546f18b828
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-05 22:40:53 +0200

    Create language picker.

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Logo from "./logo";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
+import LanguagePicker from "./language-picker";
 
 const ICONS = [
   {
@@ -47,6 +48,7 @@ function Footer() {
         <p className="text-sm italic">
           ©Grzegorz Dubiel | {date.getFullYear()}
         </p>
+        <LanguagePicker />
       </div>
     </div>
   );
--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -1,13 +1,17 @@
 import { NAV_CONFIG } from "@/constants/website";
 import Logo from "./logo";
 import NavList from "./nav-list";
+import LanguagePicker from "./language-picker";
 
 function TopBar() {
   return (
     <>
       <div className="flex min-h-[40px] w-full flex-wrap items-center justify-center bg-[rgba(0,0,0,0.47)] p-2 shadow-md sm:min-h-[60px] [@media(min-width:282px)]:justify-between">
         <Logo />
-        <NavList navConfig={NAV_CONFIG} />
+        <div className="flex space-x-1 sm:space-x-8">
+          <NavList navConfig={NAV_CONFIG} />
+          <LanguagePicker />
+        </div>
       </div>
     </>
   );
