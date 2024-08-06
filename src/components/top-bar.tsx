a5a5d0fa39e4e6a2ebead483ecd2bd38a2488954
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-06 23:31:53 +0200

    Fix top bar styles on small screen.

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -15,7 +15,7 @@ async function TopBar() {
 
   return (
     <>
-      <div className="flex min-h-[40px] w-full flex-wrap items-center justify-center bg-[rgba(0,0,0,0.47)] p-2 shadow-md sm:min-h-[60px] [@media(min-width:282px)]:justify-between">
+      <div className="flex min-h-[40px] w-full flex-wrap items-center justify-center space-x-4 bg-[rgba(0,0,0,0.47)] p-2 shadow-md sm:min-h-[60px] [@media(min-width:410px)]:justify-between">
         <Logo />
         <div className="flex space-x-1 sm:space-x-8">
           <NavList navConfig={navConfig} />
