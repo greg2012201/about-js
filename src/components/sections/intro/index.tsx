7117221dbf00a67d3525fb1eff9ea7dc396d4318
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-06 22:41:43 +0200

    Add translations for home.

--- src/components/sections/intro/index.tsx
+++ src/components/sections/intro/index.tsx
@@ -5,8 +5,11 @@ import { motion } from "framer-motion";
 import AnimatedIcons from "./animated-icons";
 import BaseText from "@/components/base-text";
 import { Subtitle, Title } from "@/components/brand-title";
+import { useTranslations } from "next-intl";
 
 function Intro() {
+  const t = useTranslations("Home");
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
@@ -15,14 +18,11 @@ function Intro() {
       className="mb-20 flex h-[calc(100vh-55px)] max-w-[930px] flex-col justify-around pb-4 [@media(min-width:660px)]:h-[calc(100vh-75px)]"
     >
       <header className="space-y-2">
-        <Title> What the hack is about.js?</Title>
-        <Subtitle>{`Tech blog about:`}</Subtitle>
+        <Title>{t("title")}</Title>
+        <Subtitle>{t("subtitle")}</Subtitle>
       </header>
       <AnimatedIcons />
-      <BaseText>
-        👋 My name is Greg and I am here to help you to keep up with the fresh
-        news from JS land.
-      </BaseText>
+      <BaseText>{t("introduction")}</BaseText>
     </motion.div>
   );
 }
