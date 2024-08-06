51e256743806fb8ee69f90d8153440719d2866c3
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-06 22:31:37 +0200

    Include translation in nav config.

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -2,14 +2,23 @@ import { NAV_CONFIG } from "@/constants/website";
 import Logo from "./logo";
 import NavList from "./nav-list";
 import LanguagePicker from "./language-picker";
+import { getLocale } from "next-intl/server";
+import { Locale } from "@/types";
 
-function TopBar() {
+async function TopBar() {
+  const locale = (await getLocale()) as Locale;
+  const navConfig = NAV_CONFIG.get(locale);
+
+  if (!navConfig) {
+    throw new Error(`Invalid locale configuration!`);
+  }
+
   return (
     <>
       <div className="flex min-h-[40px] w-full flex-wrap items-center justify-center bg-[rgba(0,0,0,0.47)] p-2 shadow-md sm:min-h-[60px] [@media(min-width:282px)]:justify-between">
         <Logo />
         <div className="flex space-x-1 sm:space-x-8">
-          <NavList navConfig={NAV_CONFIG} />
+          <NavList navConfig={navConfig} />
           <LanguagePicker />
         </div>
       </div>
