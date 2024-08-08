73a108b8efa524dc6db4852b976572b0c1700bc5
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-08 23:59:14 +0200

    Fix build.

--- src/app/[locale]/posts/page.tsx
+++ src/app/[locale]/posts/page.tsx
@@ -3,12 +3,18 @@ import PostList from "@/components/post-list";
 import SectionWrapper from "@/components/section-wrapper";
 import { PAGES_META } from "@/constants/website";
 import { getTranslations } from "next-intl/server";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 export async function generateMetadata() {
   return PAGES_META.get("posts");
 }
 
-async function Posts() {
+type Props = {
+  params: { locale: string };
+};
+
+async function Posts({ params: { locale } }: Props) {
+  unstable_setRequestLocale(locale);
   const t = await getTranslations("PostList");
   return (
     <div className="flex flex-col items-center space-y-4 py-4">
