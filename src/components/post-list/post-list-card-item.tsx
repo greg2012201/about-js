6b3547752e05e2cdc4d22f9dc511f00b63d157f0
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-08 23:40:35 +0200

    Add translation for post list item.

--- src/components/post-list/post-list-card-item.tsx
+++ src/components/post-list/post-list-card-item.tsx
@@ -3,11 +3,13 @@ import Profile from "../blog/profile";
 import Title from "../title";
 import BaseText from "../base-text";
 import { Post } from "@/lib/posts";
+import { getTranslations } from "next-intl/server";
 
 type PostListCardItemProps = Pick<Post, "data" | "excerpt">;
 
-function PostListCardItem({ data, excerpt }: PostListCardItemProps) {
+async function PostListCardItem({ data, excerpt }: PostListCardItemProps) {
   const { title, slug, author, authorAvatar, createdAt, description } = data;
+  const t = await getTranslations("PostListCardItem");
 
   return (
     <li className="mx-auto flex min-h-[225px] max-w-[400px] flex-col rounded-md bg-slate-800 shadow sm:max-w-[650px] sm:flex-row">
@@ -25,7 +27,7 @@ function PostListCardItem({ data, excerpt }: PostListCardItemProps) {
           className=" self-end text-pink-300 hover:underline"
           href={`posts/${slug}`}
         >
-          Read more...
+          {t("readMoreLink")}
         </Link>
       </div>
     </li>
