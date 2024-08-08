591bef822840a1f3405f1650b9c0f5819d827825
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-08 23:36:35 +0200

    Add translation for post list page.

--- src/app/[locale]/posts/page.tsx
+++ src/app/[locale]/posts/page.tsx
@@ -2,15 +2,17 @@ import { Title } from "@/components/brand-title";
 import PostList from "@/components/post-list";
 import SectionWrapper from "@/components/section-wrapper";
 import { PAGES_META } from "@/constants/website";
+import { getTranslations } from "next-intl/server";
 
 export async function generateMetadata() {
   return PAGES_META.get("posts");
 }
 
-function Posts() {
+async function Posts() {
+  const t = await getTranslations("PostList");
   return (
     <div className="flex flex-col items-center space-y-4 py-4">
-      <Title className="mb-12">Posts</Title>
+      <Title className="mb-12">{t("title")}</Title>
       <SectionWrapper className="h-fit">
         <PostList />
       </SectionWrapper>
