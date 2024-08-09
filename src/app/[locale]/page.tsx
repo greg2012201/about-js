d726e299ec39f411c001c80f45abb6bec17d6c67
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-09 22:54:07 +0200

    Translate home page metadata.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,10 +1,10 @@
 import Intro from "@/components/sections/intro";
 import PostList from "@/components/sections/post-list";
 import React from "@/components/sections/react";
-import { PAGES_META } from "@/constants/website";
+import getMetadataTranslation from "@/lib/getMetadataTranslation";
 
 export async function generateMetadata() {
-  return PAGES_META.get("home");
+  return getMetadataTranslation("Home");
 }
 
 export default function Home() {
