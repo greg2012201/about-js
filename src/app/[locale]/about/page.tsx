afe826e84319e33fac2d976fbb0b72a05f064cbe
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-09 23:03:12 +0200

    Remove empty import.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,8 +1,6 @@
 import React from "@/components/sections/react";
 import SimpleView from "./_views/simple-view";
-import { PAGES_META } from "@/constants/website";
 import getMetadataTranslation from "@/lib/getMetadataTranslation";
-
 
 export async function generateMetadata() {
   return getMetadataTranslation("About");
