c18806bcfcd7bf3b042caf7b789609e20ccc93fa
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-09 23:01:57 +0200

    Cleanup.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -3,15 +3,12 @@ import SimpleView from "./_views/simple-view";
 import { PAGES_META } from "@/constants/website";
 import getMetadataTranslation from "@/lib/getMetadataTranslation";
 
-type Props = {
-  params: { locale: string };
-};
 
 export async function generateMetadata() {
   return getMetadataTranslation("About");
 }
 
-function About({ params: { locale } }: Props) {
+function About() {
   return <SimpleView />;
 }
 
