cca81baf7c640b2062afc36de5c318f1750be7d4
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-09 23:00:23 +0200

    Transalte about page metadata.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,13 +1,14 @@
 import React from "@/components/sections/react";
 import SimpleView from "./_views/simple-view";
 import { PAGES_META } from "@/constants/website";
+import getMetadataTranslation from "@/lib/getMetadataTranslation";
 
 type Props = {
   params: { locale: string };
 };
 
 export async function generateMetadata() {
-  return PAGES_META.get("about");
+  return getMetadataTranslation("About");
 }
 
 function About({ params: { locale } }: Props) {
