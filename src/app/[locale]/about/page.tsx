45bc9aa42c84fbdba0d3a5e073b2d37ff999d8a0
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-10 22:19:00 +0200

    Fix build about page with intl.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,12 +1,20 @@
 import React from "@/components/sections/react";
 import SimpleView from "./_views/simple-view";
 import getMetadataTranslation from "@/lib/getMetadataTranslation";
+import { unstable_setRequestLocale } from "next-intl/server";
+
 
 export async function generateMetadata() {
   return getMetadataTranslation("About");
 }
 
-function About() {
+type Props = {
+  params: { locale: string };
+};
+
+
+function About({ params: { locale } }: Props) {
+  unstable_setRequestLocale(locale);
   return <SimpleView />;
 }
 
