30b77d0e64c3e2a168a81e3c4de402734fb14c44
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-11 00:23:41 +0200

    Handle lastModified in sitemap.

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,19 +1,41 @@
 import { MetadataRoute } from "next";
 import { getPathname } from "@/navigation";
 import { DEFAULT_LOCALE, host, LOCALES, pathnames } from "@/next-intl-config";
-import { getAllPostSlugs } from "@/lib/posts";
+import getPosts, {
+  getAllPostSlugs,
+  getPostsCreatedAt,
+  Post,
+  PostStorageManager,
+} from "@/lib/posts";
+import dayjs from "dayjs";
 
 function getUrl(key: keyof typeof pathnames, locale: (typeof LOCALES)[number]) {
   const pathname = getPathname({ locale, href: key });
   return `${host}/${locale}${pathname === "/" ? "" : pathname}`;
 }
+
+function convertDateFormat(dateStr: string): string {
+  const date = dayjs(dateStr, "DD-MM-YYYY");
+
+  return date.format("YYYY-MM-DD");
+}
+
 export default async function sitemap() {
   const keys = Object.keys(pathnames) as Array<keyof typeof pathnames>;
   const postsKeys = (await getAllPostSlugs(DEFAULT_LOCALE)).map(
     (slug) => `/posts/${slug}`,
   );
+
+  const posts = await getPosts();
+  const postStorage = new PostStorageManager(posts);
+
   return [...keys, ...postsKeys].map((key) => ({
-    lastModified: new Date(),
+    lastModified: postsKeys.includes(key)
+      ? postStorage.getPostDate(
+          postStorage.extractPostSlugFromString(key),
+          convertDateFormat,
+        )
+      : dayjs().format("YYYY-MM-DD"),
     url: getUrl(key, DEFAULT_LOCALE),
     alternates: {
       languages: Object.fromEntries(
