83569b6fd12b60c8549bfb59a5981386272bf1e7
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-26 23:43:18 +0200

    Add author to the og image and update og image styles.

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -4,6 +4,13 @@ import { ImageResponse } from "next/og";
 export const runtime = "edge";
 
 export default async function Image() {
+  const latoItalic = fetch(
+    new URL("/public/Lato-LightItalic.ttf", import.meta.url),
+  ).then((res) => res.arrayBuffer());
+  const latoBoldItalic = fetch(
+    new URL("/public/Lato-BoldItalic.ttf", import.meta.url),
+  ).then((res) => res.arrayBuffer());
+
   try {
     return new ImageResponse(
       (
@@ -15,15 +22,50 @@ export default async function Image() {
             display: "flex",
             alignItems: "center",
             justifyContent: "center",
+            color: "white",
+            fontSize: "60px",
+            padding: "0 40px",
           }}
         >
-          <img
-            src={`${BASE_URL}/logo.svg`}
-            alt="about.js logo"
-            title="about.js"
-          />
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "flex-end",
+            }}
+          >
+            <img
+              src={`${BASE_URL}/logo.svg`}
+              alt="about.js logo"
+              title="about.js"
+            />
+            <p style={{ marginTop: "-12px", paddingRight: "40px" }}>
+              by{" "}
+              <span
+                style={{ marginLeft: "12px", fontFamily: "Lato-BoldItalic" }}
+              >
+                Grzegorz Dubiel
+              </span>
+            </p>
+          </div>
         </div>
       ),
+      {
+        fonts: [
+          {
+            name: "Lato",
+            data: await latoItalic,
+            style: "normal",
+            weight: 200, // should be thin
+          },
+          {
+            name: "Lato-BoldItalic",
+            data: await latoBoldItalic,
+            style: "normal",
+            weight: 400,
+          },
+        ],
+      },
     );
   } catch (e: any) {
     return new Response("Failed to generate OG image", { status: 500 });
