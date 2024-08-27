f33b9fcd913b6b8f46c5b28baeb91cd3653d07b5
Grzegorz Dubiel <greg2012201@gmail.com>
2024-08-27 22:56:37 +0200

    Fix an issue with clicking an elemend behind a portal.

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -76,10 +76,22 @@ SelectScrollDownButton.displayName =
 const SelectContent = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof SelectPrimitive.Content>
->(({ className, children, position = "popper", ...props }, ref) => (
+>(({ className, children, position = "popper", ...props }, forwardedRef) => (
   <SelectPrimitive.Portal>
     <SelectPrimitive.Content
-      ref={ref}
+      ref={(instance) => {
+        if (typeof forwardedRef === "function") {
+          forwardedRef(instance);
+        } else if (forwardedRef) {
+          forwardedRef.current = instance;
+        }
+        if (!instance) {
+          return;
+        }
+        instance.ontouchstart = (e) => {
+          e.preventDefault();
+        };
+      }}
       className={cn(
         "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border bg-popover text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
         position === "popper" &&
@@ -127,6 +139,10 @@ const SelectItem = React.forwardRef<
       "relative flex w-full cursor-default select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
       className,
     )}
+    onClick={(e) => {
+      e.stopPropagation();
+      return props.onClick;
+    }}
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
