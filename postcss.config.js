fecb0c3305b82f041f71efce45487eae8e8f00d1
Grzegorz Dubiel <greg2012201@gmail.com>
2024-09-01 00:35:23 +0200

    Update post.css config to work with purgeCss

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,35 @@
-module.exports = {
-  plugins: {
-    "tailwindcss/nesting": {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+// postcss.config.js
+import tailwindcss from "tailwindcss";
+import postcssNested from "postcss-nested";
+import { default as purgecss } from "@fullhuman/postcss-purgecss";
+
+const config = {
+  plugins: [
+    "postcss-flexbugs-fixes",
+    [
+      "postcss-preset-env",
+      {
+        autoprefixer: {
+          flexbox: "no-2009",
+        },
+        stage: 3,
+        features: {
+          "custom-properties": false,
+        },
+      },
+    ],
+    tailwindcss,
+    postcssNested,
+    process.env.NODE_ENV === "production"
+      ? purgecss({
+          content: ["./src/app/**/*.{js,jsx,ts,tsx}"],
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+          safelist: {
+            standard: ["html", "body"],
+          },
+        })
+      : [],
+  ],
 };
+
+export default config;
