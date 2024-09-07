9f5e7325d54000596e9ecc0e5ac1e80f64f3088f
Grzegorz Dubiel <greg2012201@gmail.com>
2024-09-07 22:32:27 +0200

    Fix intl.

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,12 +2,18 @@ import Intro from "@/components/sections/intro";
 import PostList from "@/components/sections/post-list";
 import React from "@/components/sections/react";
 import composeMetadata from "@/lib/compose-metadata";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 export async function generateMetadata() {
   return composeMetadata({ canonical: "/", intlNamespace: "Home" });
 }
 
-export default function Home() {
+type Props = {
+  params: { locale: string };
+};
+
+export default function Home({ params: { locale } }: Props) {
+  unstable_setRequestLocale(locale);
   return (
     <div className="mx-auto flex w-full flex-col items-center overflow-hidden p-4 pb-0 text-white">
       <Intro />
