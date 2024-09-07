08e9c57a8dbaed7d5f2bfee84176ff6bcbba6758
Grzegorz Dubiel <greg2012201@gmail.com>
2024-09-07 22:41:27 +0200

    Delete redundant code.

--- src/components/section-wrapper.tsx
+++ src/components/section-wrapper.tsx
@@ -1,47 +1,32 @@
 "use client";
 
-import { ReactElement, ReactNode } from "react";
+import { ReactNode } from "react";
 import { motion } from "framer-motion";
 import { ClassNameValue, twMerge } from "tailwind-merge";
 
 type Props = {
   children: ReactNode;
   className?: ClassNameValue;
-  renderIconLine?: ReactElement | null;
   iconsLineWrapperClassName?: ClassNameValue;
 };
 
-function SectionWrapper({
-  children,
-  className,
-  renderIconLine = null,
-  iconsLineWrapperClassName,
-}: Props) {
+function SectionWrapper({ children, className }: Props) {
   return (
-    <section className="flex w-full max-w-[930px]">
-      <div
-        className={twMerge(
-          "z-20 flex h-[650px] w-full flex-col space-y-6",
-          className,
-        )}
-      >
-        <motion.div
-          initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ delay: 0.3 }}
-          viewport={{ once: true }}
-          className="flex h-full flex-col space-y-6"
-        >
-          {children}
-        </motion.div>
-      </div>
-      {renderIconLine && (
-        <div
-          className={twMerge(`h-[650px] w-[40px] `, iconsLineWrapperClassName)}
-        >
-          {renderIconLine}
-        </div>
+    <section
+      className={twMerge(
+        "z-20 flex h-[650px] w-full max-w-[930px] flex-col space-y-6",
+        className,
       )}
+    >
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ delay: 0.3 }}
+        viewport={{ once: true }}
+        className="flex h-full flex-col space-y-6"
+      >
+        {children}
+      </motion.div>
     </section>
   );
 }
