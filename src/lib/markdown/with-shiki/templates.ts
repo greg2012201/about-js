ef3b42a54a219ad0c34869453bd3d9aea0293380
Grzegorz Dubiel <greg2012201@gmail.com>
2024-10-01 21:16:19 +0200

    Update style for shiki container.

--- src/lib/markdown/with-shiki/templates.ts
+++ src/lib/markdown/with-shiki/templates.ts
@@ -30,14 +30,14 @@ export const headerTemplate = ({ lang, codeBlockId }: HeaderProps) => {
   const renderIcon = LANG_ICONS_MAP.get(lang);
   const buttonId = short.generate();
   return dedent`
-  <div class="header flex items-center border-b-[1px] border-slate-500 px-6 py-1 text-neutral-400">
+  <div class="header flex items-center border-b-[1px] border-slate-500 px-6 text-neutral-400">
     <div class="flex flex-grow items-center space-x-1">
       ${renderIcon}
       <p class="text-sm font-semibold capitalize ">
           ${transformLang(lang)}
       </p>    
     </div>
-    <button id="${buttonId}" class="copy-button h-[30px]" onclick="handleCopy('${codeBlockId}', '${buttonId}')" type="button">
+    <button id="${buttonId}" class="copy-button h-[24px]" onclick="handleCopy('${codeBlockId}', '${buttonId}')" type="button">
       ${mdOutlineDone}${faCopy}
     </button>
   </div>
@@ -64,7 +64,7 @@ export const wrapperTemplate = ({
   <div class="flex flex-col rounded-md border-[1px] border-slate-500 bg-[#0f111a] my-6">
     ${headerTemplate({ lang, codeBlockId })}
     <div id="${codeBlockId}" class="${twMerge(
-      "p-2 text-sm [&>pre]:overflow-x-auto [&>pre]:p-4 [&>pre]:leading-snug  [&_code]:block [&_code]:max-w-[100px]",
+      "px-2 text-sm [&>pre]:overflow-x-auto [&>pre]:px-2 [&>pre]:leading-snug [&_code]:block [&_code]:max-w-[100px]",
       lineNumbersclass,
     )}">${children}</div>
   </div>
