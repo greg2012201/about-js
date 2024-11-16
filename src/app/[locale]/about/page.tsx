645933cfde771c898586707c6dde71e9a2b3d70f
Grzegorz Dubiel <greg2012201@gmail.com>
2024-11-16 23:34:14 +0100

    Add one more section to the about.

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -6,6 +6,7 @@ import WithBackgroundIcons from "@/components/with-backround-icons";
 import composeMetadata from "@/lib/compose-metadata";
 import { Link } from "@/navigation";
 import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import { RiRobot2Line } from "react-icons/ri";
 
 export async function generateMetadata() {
   return composeMetadata({ canonical: "/about", intlNamespace: "About" });
@@ -53,10 +54,22 @@ async function About({ params: { locale } }: Props) {
               alt: "logos of the frontend technologies",
             }}
           />
+          <ContentContainer
+            title={t("webAI.title")}
+            content={t("webAI.content")}
+            orientation="right"
+            renderImage={
+              <RiRobot2Line
+                width={175}
+                height={100}
+                className="row-span-2 w-[175px] self-center text-[400px] text-[#61dafb] md:w-full md:pr-4"
+              />
+            }
+          />
 
           <div className="z-20">
             <Link
-              className="w-full text-center text-2xl text-pink-100 hover:text-pink-200 sm:text-3xl"
+              className="col-start-2 w-full text-center text-2xl text-pink-100 hover:text-pink-200 sm:text-3xl"
               href="/posts"
             >
               {t("linkSection.link")}
