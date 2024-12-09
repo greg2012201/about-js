8d5b83651297c1cb82c8e00ff4dcc99426992284
Grzegorz Dubiel <greg2012201@gmail.com>
2024-12-09 21:51:37 +0100

    Fix layout.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -10,17 +10,21 @@ export const metadata: Metadata = {
 
 function NotFound() {
   return (
-    <div className="relative flex min-h-screen items-center justify-center bg-gradient-to-b from-[#141e30] via-[#243b55] to-[#243b55] text-white">
-      <div className="flex flex-col items-center justify-center space-y-20 p-4">
-        <Title className="text-[80px] font-bold">404</Title>
-        <Subtitle className="pb-1">
-          Ups! Something went wrong! Content could not be found...
-        </Subtitle>
-        <Link href="/">
-          <BaseText>Go to the home page 👉</BaseText>
-        </Link>
-      </div>
-    </div>
+    <html lang="en">
+      <body>
+        <div className="relative flex min-h-screen items-center justify-center bg-gradient-to-b from-[#141e30] via-[#243b55] to-[#243b55] text-white">
+          <div className="flex flex-col items-center justify-center space-y-20 p-4">
+            <Title className="text-[80px] font-bold">404</Title>
+            <Subtitle className="pb-1">
+              Ups! Something went wrong! Content could not be found...
+            </Subtitle>
+            <Link href="/">
+              <BaseText>Go to the home page 👉</BaseText>
+            </Link>
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
 
