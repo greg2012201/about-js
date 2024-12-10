72add8a6e0fe943f35a658d22770b0be66f22634
Grzegorz Dubiel <greg2012201@gmail.com>
2024-12-10 21:19:55 +0100

    Use correct url.

--- src/next-intl-config.ts
+++ src/next-intl-config.ts
@@ -13,9 +13,10 @@ export const pathnames: Pathnames<typeof LOCALES> = {
 export const localePrefix: LocalePrefix<typeof LOCALES> = "always";
 
 export const port = process.env.PORT || 3000;
-export const host = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : `http://localhost:${port}`;
+export const host =
+  process.env.NODE_ENV === "production"
+    ? `https://${process.env.SITE_URL}`
+    : `http://localhost:${port}`;
 
 type LocaleMap = {
   [key in Locale]: `/${key}`;
