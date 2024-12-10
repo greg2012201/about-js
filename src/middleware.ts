0e6cbc40e94f98d019809ac2f75b63bbe1ffcc45
Grzegorz Dubiel <greg2012201@gmail.com>
2024-12-10 21:20:06 +0100

    Fix og image.

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,5 +7,5 @@ export default createMiddleware({
   defaultLocale: DEFAULT_LOCALE,
 });
 export const config = {
-  matcher: ["/((?!api|_next|.*\\..*).*)"],
+  matcher: ["/((?!api|_next|opengraph-image|.*\\..*).*)"],
 };
