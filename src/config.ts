5e2bf375d5f046289ee8cb0a316cbbb904773ece
Grzegorz Dubiel <greg2012201@gmail.com>
2024-12-10 07:10:57 +0100

    Update config.

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const BASE_URL = process.env.VERCEL_URL
-  ? `https://${process.env.SITE_URL}`
-  : `http://localhost:${process.env.PORT || 3000}`;
+export const BASE_URL =
+  process.env.NODE_ENV === "production"
+    ? `https://${process.env.SITE_URL}`
+    : `http://localhost:${process.env.PORT || 3000}`;
