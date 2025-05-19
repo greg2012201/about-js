bfc58d2d5de2591fa16dfbec481dc5b5fbb8d62e
Grzegorz Dubiel <greg2012201@gmail.com>
2025-05-19 14:52:37 +0200

    Enhance robots.

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,7 +7,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: [ "/api/", "/opengraph-image"],
+        disallow: ["/api/", "/_next/"],
       },
     ],
     sitemap: `${BASE_URL}/sitemap.xml`,
